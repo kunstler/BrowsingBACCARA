10c492621a5cbf66fba92cae28824ceafa15a670
kunstler <georges.kunstler@gmail.com>
2020-05-11 15:30:19 +0200

    add analysis in drake

--- R/Functions.R
+++ R/Functions.R
@@ -34,7 +34,7 @@ fun_site_browsing_rate <- function(site_alt, df){
 }
 
 
-Brows_Prob_CI <- function(bdd){
+Brows_Prob_CI <- function(bdd, vec_sp){
   vec_site_alt <- unique(bdd$site_alt)
   list_res_ci <- vector("list")
   for(i in 1:4){
@@ -80,3 +80,206 @@ Pred_Brows_Prob <- function(fglm, var_l, var_h, HT_val, df, sp, var = "Tm_hiv"){
   return(df_out)
 }
 
+format_browsing_rate <- function(bdd, upi){
+  bdd$site_alt <- paste0(bdd$Site, bdd$Z)
+  bdd$SpeciesUPI <- "Deciduous"
+  bdd$SpeciesUPI[bdd$Species == "ABAL"] <- "Fir"
+  bdd$SpeciesUPI[bdd$Species == "PIAB"] <- "Spruce"
+  
+  # bdd$Z.Classe<-NA # This is a bit problematic we don 't use the same classe of elevation between the growth analysis and the browsing rate analysis
+  # bdd[bdd$Site%in%c('Belledonne','Vercors'),]$Z.Classe<-
+  #   substr(bdd[bdd$Site%in%c('Belledonne','Vercors'),]$Plot,1,1)
+  # bdd[bdd$Site=='Chartreuse',]$Z.Classe<-substr(bdd[bdd$Site=='Chartreuse',]$Plot,1,2)
+  # bdd[bdd$Site=='Freiburg',]$Z.Classe<-substr(bdd[bdd$Site=='Freiburg',]$Plot,3,3)
+  # bdd[bdd$Site%in%c('Kroscienko','Gallivare'),]$Z.Classe<-
+  #   substr(bdd[bdd$Site%in%c('Kroscienko','Gallivare'),]$Plot,2,2)
+  # bdd[bdd$Site=='Bavarian FNP',]$Z.Classe<-substr(bdd[bdd$Site=='Bavarian FNP',]$Plot,1,4) # This is problematic because we have a random effect per plot and very few indiv (mostly 1) per plot
+  bdd$Tm_hiv<-(bdd$tm_new_dec+bdd$tm_new_jan+bdd$tm_new_fev)/3
+  bdd$Tm_hiv_CR <- scale(bdd$Tm_hiv)
+  bdd$HT_CR <- scale(bdd$HT)
+  bdd <-  bdd[ !is.na(bdd$HT) & !is.na(bdd$Tm_hiv) & !is.na(bdd$B) & !is.na(bdd$Site) & !is.na(bdd$Z.Classe), ]
+  bdd$Z_F <- factor(bdd$Z.Classe)
+  return(bdd)
+}
+
+format_browsing_rate_NoGalliv <- function(bdd){
+ bddsg <- bdd[!bdd$Site=='Gallivare',]
+ bddsg$site_alt <- paste0(bddsg$Site, bddsg$Z)
+ bddsg$Tm_hiv_CR <- scale(bddsg$Tm_hiv)
+ bddsg$HT_CR <- scale(bddsg$HT)
+ bddsg <-  bddsg[ !is.na(bddsg$HT) & !is.na(bddsg$Tm_hiv) & !is.na(bddsg$B) & !is.na(bddsg$Site) & !is.na(bddsg$Z.Classe), ]
+ bddsg$Z_F <- factor(bddsg$Z.Classe)
+ 
+ 
+ return(bddsg)
+}
+
+
+run_analysis_browsing_rate <- function(bdd, vec_sp){
+  list_res <- lapply(vec_sp, AICmod1, bdd)
+  return(list_res)
+}
+
+get_coef_browsing_rate <- function(list_res, vec_sp){
+  list_coef5 <- lapply(list_res, FUN = function(x) fixef(x[[5]]))
+  names(list_coef5) <- vec_sp
+  return(list_coef5)
+}
+
+format_table_coef_br <- function(list_res, vec_sp){
+  list_coefT <- lapply(list_res, FUN = function(x) summary(x[[5]])$coefficients) 
+  names(list_coefT) <- vec_sp
+  return(list_coefT)
+}
+
+get_AIC_browsing_rate <- function(list_res, vec_sp){
+  mat_aic <- t(sapply(list_res, FUN = function(x) sapply(x, FUN = function(z) AIC(z))))
+  rownames(mat_aic)<- vec_sp
+  return(mat_aic)
+}
+
+get_deltaAIC_browsing_rate <- function(mat_aic){
+  delta_aic <- mat_aic -apply(mat_aic, MARGIN = 1, min)
+  return(delta_aic)
+}
+
+get_browsing_rate_per_site <- function(bdd, vec_sp){
+  df_ci <- Brows_Prob_CI(bdd, vec_sp)
+  df_ci$sp<-gsub('ABAL','A. alba',df_ci$sp)
+  df_ci$sp<-gsub('ACPS','A. pseudoplatanus',df_ci$sp)
+  df_ci$sp<-gsub('FASY','F. sylvatica',df_ci$sp)
+  df_ci$sp<-gsub('PIAB','P. abies',df_ci$sp)
+  return(df_ci)
+}
+
+get_pred_ci_browsing_rate <- function(list_res, bdd, vec_sp){
+  list_pred <- vector("list")
+  for (i in 1:4){
+    list_pred[[i]]<- Pred_Brows_Prob(list_res[[i]][[5]],
+                                     var_l = quantile(bdd$Tm_hiv[bdd$Species == vec_sp[i]], probs = 0.05),
+                                     var_h = quantile(bdd$Tm_hiv[bdd$Species == vec_sp[i]], probs = 0.95),
+                                     HT_val = quantile(bdd$HT, probs = c(0.05, 0.95)),
+                                     df = bdd, sp = vec_sp[i], var = "Tm_hiv")
+  }
+  pred_ci <- as.data.frame(do.call("rbind", list_pred))
+  pred_ci$sp <- pred_ci$Species
+  pred_ci$sp<-gsub('ABAL','A. alba',pred_ci$sp)
+  pred_ci$sp<-gsub('ACPS','A. pseudoplatanus',pred_ci$sp)
+  pred_ci$sp<-gsub('FASY','F. sylvatica',pred_ci$sp)
+  pred_ci$sp<-gsub('PIAB','P. abies',pred_ci$sp)
+  pred_ci$mean <- pred_ci$y
+  pred_ci$HT <- factor(pred_ci$HT)
+  return(pred_ci)
+}
+
+
+
+plot_browsing_rate <- function(df_ci, pred_ci){
+  ggplot(df_ci,aes(Tm_hiv,mean))+geom_point()+facet_grid(sp~.)+
+    geom_errorbar(aes(ymin=lwr,ymax=upr))+
+    geom_line(data=pred_ci, aes(x = Tm_hiv, y = mean, col = HT),size=0.7)+
+    scale_colour_manual(values=c('chocolate1','chocolate4'))+
+    geom_ribbon(data=pred_ci,aes(ymin=plo,ymax=phi, fill = HT),alpha=0.3)+
+    scale_fill_manual(values=c('chocolate1','chocolate4'))+
+    labs(fill='Height (cm)',col='Height (cm)')+
+    xlab('Tm_wint (°C)')+ylab('Mean predicted browsing probability')+
+    theme_bw()+theme(strip.background = element_rect(fill='grey95'))
+  
+  ggsave("figures/BrowsingProbaALL.png", width = 14, height = 18, units = "cm")
+}
+
+plot_browsing_rate_PA_NoGalliv <- function(df_ci, pred_ci){
+  ggplot(df_ci[df_ci$sp=='P. abies',],aes(Tm_hiv,mean))+geom_point()+facet_grid(sp~.)+
+    geom_errorbar(aes(ymin=lwr,ymax=upr))+
+    geom_line(data=pred_ci[pred_ci$sp=='P. abies',], aes(x = Tm_hiv, y = mean, col = HT),size=0.7)+
+    scale_colour_manual(values=c('chocolate1','chocolate4'))+
+    geom_ribbon(data=pred_ci[pred_ci$sp=='P. abies',],aes(ymin=plo,ymax=phi, fill = HT),alpha=0.3)+
+    scale_fill_manual(values=c('chocolate1','chocolate4'))+
+    labs(fill='Height (cm)',col='Height (cm)')+
+    xlab('Tm_wint (°C)')+ylab('Mean predicted browsing probability')+
+    theme_bw()+theme(strip.background = element_rect(fill='grey95'))
+  
+  ggsave("figures/BrowsingProba_PA_NoGalliv.png", width = 14, height = 11, units = "cm")
+}
+
+# Growth
+format_growth <- function(bdd){
+  bdd$B<-as.factor(bdd$B)
+  bdd$Press.ong<-as.numeric(as.character(bdd$Press.ong))
+  bdd$Thiv09_10 <- bdd$Tm_hiv
+  return(bdd)
+}
+
+
+fit_growth_sp <- function(sp, bdd){
+  df_t<-na.omit(bdd[bdd$Species==sp,c("LLS","HT","Tmean456","Site", "Z.Classe", "B")])
+  model_growth<-lmer(log(LLS+1)~scale(HT)+scale(Tmean456)*B+(1|Site/Z.Classe),data=df_t)
+  return(model_growth)  
+}
+
+
+run_analysis_growth <- function(bdd, vec_sp){
+  list_res <- lapply(vec_sp, fit_growth_sp, bdd)
+  return(list_res)
+}
+
+format_coef_growth <- function(list_res,vec_sp){
+  vec_spl <- c('Abies','Acer','Fagus','Picea')
+  names(vec_spl) <- c("ABAL", "ACPS", "FASY", "PIAB")
+  prfig2<-expand.grid(c('HT','Tmean456','B','Tmean456:B'),
+                      c('Abies','Acer','Fagus','Picea'))  
+  names(prfig2)<-c('Effet','Espece')
+  prfig2$Estimate<-NA
+  prfig2$Std.err<-NA
+  prfig2$Pvalue<-NA
+  
+  for (i in 1:4){
+    for (j in 1:4){
+      prfig2[prfig2$Espece== vec_spl[vec_sp[i]],]$Estimate[j] <- summary(list_res[[i]])$coefficients[j+1, 1]
+      prfig2[prfig2$Espece== vec_spl[vec_sp[i]],]$Std.err[j]  <- summary(list_res[[i]])$coefficients[j+1, 2]
+      prfig2[prfig2$Espece== vec_spl[vec_sp[i]],]$Pvalue[j]  <- summary(list_res[[i]])$coefficients[j+1, 5]
+    }
+  }
+  
+  prfig2$lower<-prfig2$Estimate-2*prfig2$Std.err  # 2* = pour calcul IC
+  prfig2$upper<-prfig2$Estimate+2*prfig2$Std.err
+  prfig2$Effet<-gsub('Tmean456','Tm_spring',prfig2$Effet)
+  prfig2$Effet<-gsub('B','Browsing',prfig2$Effet)
+  prfig2$Effet<-gsub('HT','Height',prfig2$Effet)
+  prfig2$Espece<-gsub('Abies','A. alba',prfig2$Espece)
+  prfig2$Espece<-gsub('Acer','A. pseudoplatanus',prfig2$Espece)
+  prfig2$Espece<-gsub('Fagus','F. sylvatica',prfig2$Espece)
+  prfig2$Espece<-gsub('Picea','P. abies',prfig2$Espece)
+  return(prfig2)
+}
+
+
+plot_growth <- function(prfig2){
+  ggplot(prfig2,
+         aes(Estimate,factor(Effet,levels=c('Height','Tm_spring:Browsing','Browsing','Tm_spring')),
+             color=factor(Effet,levels=c('Tm_spring','Browsing','Tm_spring:Browsing','Height'))))+
+    geom_point()+
+    facet_grid(Espece~.,scales='free_y',space='free_y')+
+    geom_errorbarh(aes(xmin=lower, xmax=upper),height=.4)+geom_vline(xintercept = 0)+
+    theme_bw()+xlab('Estimate and CI95%')+ylab('Effect')+
+    theme(strip.background = element_rect(fill='grey95'))+labs(color='Effect')+
+    scale_color_manual(values=c('darkorange','forestgreen','chocolate4','black'))
+  
+  ggsave("figures/GrowthAllSp.png", width = 14, height = 18, units = "cm")
+}
+
+
+# FIT SANS Gallivard
+
+plot_growth_PA_SG<- function(prfig2){
+  ggplot(prfig2[prfig2$Espece=='P. abies',],
+         aes(Estimate,factor(Effet,levels=c('Height','Tm_spring:Browsing','Browsing','Tm_spring')),
+             color=factor(Effet,levels=c('Tm_spring','Browsing','Tm_spring:Browsing','Height'))))+
+    geom_point()+
+    geom_errorbarh(aes(xmin=lower, xmax=upper),height=.4)+geom_vline(xintercept = 0)+
+    theme_bw()+xlab('Estimate and CI95%')+ylab('Effect')+
+    theme(strip.background = element_rect(fill='grey95'))+labs(color='Effect')+
+    scale_color_manual(values=c('darkorange','forestgreen','chocolate4','black'))
+
+  ggsave("figures/Growth_PA_NoGalliv.png", width = 15, height = 10, units = "cm")
+}
--- plan.R
+++ plan.R
@@ -10,15 +10,42 @@ library(ade4)
 library(factoextra)
 require(dplyr)
 require(drake)
+library(lme4)
 
 # source all files
 source(file.path("R", "Functions.R"))
 
+dir.create("figures", showWarnings = FALSE)
+
 ### plan
 
 plan <- drake_plan(
-   bdd = read.table('data/JdD_MB.csv',h=T,sep=';')
-   )
+   upi = read.table('data/UPI.csv',h=TRUE,sep=';'),
+   bdd = read.table('data/JdD_MB.csv',h=T,sep=';'),
+   vec_sp = c("ABAL", "ACPS", "FASY", "PIAB"),
+   df_br = format_browsing_rate(bdd),
+   df_br_NG = format_browsing_rate_NoGalliv(df_br), 
+   list_res_br = run_analysis_browsing_rate(df_br, vec_sp),
+   coef_br = get_coef_browsing_rate(list_res_br, vec_sp),
+   table_coef_br = format_table_coef_br(list_res_br, vec_sp),
+   AIC_br = get_AIC_browsing_rate(list_res, vec_sp),
+   DeltaAIC_br = get_deltaAIC_browsing_rate(AIC_br),
+   BR_p_site = get_browsing_rate_per_site(df_br, vec_sp),
+   pred_BR = get_pred_ci_browsing_rate(list_res_br, df_br, vec_sp),
+   plot_BR = plot_browsing_rate(BR_p_site, pred_BR),
+   list_res_br_NG = run_analysis_browsing_rate(df_br_NG, vec_sp),
+   BR_p_site_NG = get_browsing_rate_per_site(df_br_NG, vec_sp),
+   pred_BR_NG = get_pred_ci_browsing_rate(list_res_br_NG, df_br_NG, vec_sp),
+   plot_BR_PA_NG = plot_browsing_rate_PA_NoGalliv(BR_p_site_NG, pred_BR_NG),
+   df_gr = format_growth(df_br),
+   df_gr_NG = format_growth(df_br_NG),
+   list_res_gr = run_analysis_growth(df_gr, vec_sp),
+   list_res_gr_NG = run_analysis_growth(df_gr_NG, vec_sp),
+   data_coef_gr = format_coef_growth(list_res_gr, vec_sp),
+   data_coef_gr_NG = format_coef_growth(list_res_gr_NG, vec_sp),
+   plot_growth_all = plot_growth(data_coef_gr),
+   plot_growth_PA_NG = plot_growth_PA_SG(data_coef_gr_NG)
+)
 
 # Make plan
 make(plan)
